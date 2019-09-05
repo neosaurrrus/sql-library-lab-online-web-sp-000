e85668ed3fc297b9dcac76171095205cd8701775
Lukie Kang <Lukie Kang>
2019-09-05 21:43:24 +0100

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,48 @@
-INSERT into seriesa (title) VALUES ("Lord of the Rings");
+INSERT into series (title) VALUES ("Lord of the Rings");
 INSERT into series (title) VALUES ("Discworld");
+
+INSERT into subgenres (name) VALUES ("Fantasy");
+INSERT into subgenres (name) VALUES ("Comedy");
+
+INSERT into authors (name) VALUES ("Tolkien");
+INSERT into authors (name) VALUES ("Pratchett");
+
+
+
+
+INSERT into books (title, year,series_id) VALUES ("The followship of the ring", 1943, 1);
+INSERT into books (title, year,series_id) VALUES ("The too towers", 1943, 1);
+INSERT into books (title, year,series_id) VALUES ("Return of the Bing", 1943, 1);
+
+INSERT into books (title, year,series_id) VALUES ("The killer of magic", 1953, 2);
+INSERT into books (title, year,series_id) VALUES ("The fight fantastic", 1949, 2);
+INSERT into books (title, year,series_id) VALUES ("Something about witches", 1945, 2);
+
+INSERT into characters(name, motto, species, series_id, author_id) VALUES ("Aragon", "Stab things", "Human", 1,1);
+INSERT into characters(name, motto, species, series_id, author_id) VALUES ("Letgolas", "Stab things with Arrows", "Elf", 1,1);
+INSERT into characters(name, motto, species, series_id, author_id) VALUES ("Dimli", "Stab things with an Axe", "Dwarf", 1,1);
+INSERT into characters(name, motto, species, series_id, author_id) VALUES ("Tom Bimbodil", "Smoke things", "Human", 1,1);
+
+INSERT into characters(name, motto, species, series_id, author_id) VALUES ("Vincewind", "Cast magic on things", "Human", 2,2);
+INSERT into characters(name, motto, species, series_id, author_id) VALUES ("D.Eath", "Kill things", "Other", 2,2);
+INSERT into characters(name, motto, species, series_id, author_id) VALUES ("Blinky", "Carry things", "Horse", 2,2);
+INSERT into characters(name, motto, species, series_id, author_id) VALUES ("Beebo", "Eat things", "Cat", 2,2);
+
+INSERT into character_books (character_id, book_id) VALUES (1,1);
+INSERT into character_books (character_id, book_id) VALUES (1,2);
+INSERT into character_books (character_id, book_id) VALUES (1,3);
+INSERT into character_books (character_id, book_id) VALUES (2,1);
+INSERT into character_books (character_id, book_id) VALUES (2,2);
+INSERT into character_books (character_id, book_id) VALUES (2,3);
+INSERT into character_books (character_id, book_id) VALUES (3,1);
+INSERT into character_books (character_id, book_id) VALUES (4,1);
+
+INSERT into character_books (character_id, book_id) VALUES (5,4);
+INSERT into character_books (character_id, book_id) VALUES (5,5);
+INSERT into character_books (character_id, book_id) VALUES (5,6);
+INSERT into character_books (character_id, book_id) VALUES (6,4);
+INSERT into character_books (character_id, book_id) VALUES (6,5);
+INSERT into character_books (character_id, book_id) VALUES (6,6);
+INSERT into character_books (character_id, book_id) VALUES (7,1);
+INSERT into character_books (character_id, book_id) VALUES (8,2);
 
--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,67 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "
+  SELECT books.title, books.year
+  FROM books
+  INNER JOIN series ON books.series_id = series_id
+  WHERE series_id = 1
+  GROUP BY books.title
+  ORDER BY books.year 
+  "
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "
+  SELECT characters.name, characters.motto
+  FROM characters
+  ORDER BY LENGTH(characters.motto) DESC
+  LIMIT 1;
+  "
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "
+  SELECT species, COUNT(species)
+  FROM characters
+  GROUP BY species
+  ORDER BY COUNT(species) DESC
+  LIMIT 1;
+  "
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+  FROM authors
+  INNER JOIN series ON authors.id = series.author_id
+  INNER JOIN subgenres ON series.subgenre_id = subgenres.id;
+  "
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "
+  SELECT s.title
+  FROM (
+    SELECT b.series_id, COUNT(c.id)
+    FROM characters AS c
+    LEFT JOIN character_books AS cb ON c.id = cb.character_id
+    LEFT JOIN books AS b ON cb.book_id = b.id
+    WHERE c.species='human'
+    GROUP BY b.series_id
+    ORDER BY COUNT(c.id) DESC
+    LIMIT 1
+  ) AS t
+  LEFT JOIN series AS s ON s.id = t.series_id
+  ;
+  "
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "
+  SELECT c.name, count(cb.book_id)
+  FROM character_books AS cb
+  LEFT JOIN characters AS c ON c.id = cb.character_id
+  GROUP BY c.name
+  ORDER BY count(cb.book_id) DESC
+  ;
+  "
 end
